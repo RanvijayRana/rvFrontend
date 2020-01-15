4b1ab3a8ea68f32af7318b324255993c8adc19ae
mr ranvijay <ranvijay.cse@outlook.com>
2020-01-15 19:53:27 +0530

    day3of100daysofcode

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,20 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SignupComponent } from "./signup/signup.component";
+import { LoginComponent } from "./login/login.component";
 
-
-import { RouterModule,Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild([
-      {path : 'signup' , component : SignupComponent}
-    ])
+    ReactiveFormsModule,
+    RouterModule.forChild([{ path: "signup", component: SignupComponent }])
   ],
   declarations: [SignupComponent, LoginComponent]
 })
-export class UserModule { }
+export class UserModule {}
